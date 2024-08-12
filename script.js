82c928fcdcc931cdd71ea66032a89743edde9f12
Noah Adegboye <adegboyenoah@gmail.com>
2024-08-12 08:10:48 +0100

    adding the html and js files

---
+++ script.js
@@ -0,0 +1,30 @@
+let itemInput = document.getElementById('item-input');
+let addButton = document.getElementById('add-item-button');
+let list = document.getElementById('item-list');
+
+let inputValue = itemInput.value.trim();
+
+// function addItem(e) {
+
+//     if(itemValue !== "") {
+//         const listItem = document.createElement('li');
+//         listItem.textContent = itemValue;
+        
+//         list.appendChild(listItem);
+
+//         itemInput.value = '';
+//     }
+// }
+
+// addButton.addEventListener('click', addItem)
+
+
+// document.addEventListener('DOMContentLoaded', update)
+
+addButton.addEventListener('click', addItem);
+
+function addItem(e) {
+    if(itemValue !== "") {
+
+    }
+}
