32ecc65416471d6df34e577feab94b2a36eee17f
Okeke Chinedu Emmanuel <chiscookeke11@gmail.com>
2024-08-13 08:46:51 -1100

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-let input = document.getElementById('item-input');
-let addButton = document.getElementById('add-item-button');
-let list = document.getElementById('item-list');
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
 
-let inputValue = input.value.trim();
-
-addButton.addEventListener('click', addItem);
-
-function addItem(e) {
-    if(inputValue !== "") {
-        const listItem = document.createElement('li');
-        listItem.textContent = inputValue;
-
-        list.appendChild('listItem');
-
-        input.value = "";
+function addItem() {
+    if (itemInput.value === '') {
+        alert("Please add a task");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = itemInput.value;
+        itemList.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+    itemInput.value = "";
+    saveData();
 }
+
+itemList.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", itemList.innerHTML);
+}
+
+function loadData() {
+    itemList.innerHTML = localStorage.getItem("data");
+}
+
+document.getElementById("add-item-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    addItem();
+});
+
+loadData();
